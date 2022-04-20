9d6ca9476d8dd242ce560b3eee576628f7495467
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-19 21:14:24 -0500

    Arreglo a shop techin

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+// const container = document.getElementById('app');
+// const root = ReactDOM.createRoot(container);
+// root.render = (<App />);
+ReactDOM.render(<App />, document.getElementById('app'));
