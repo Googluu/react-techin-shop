c7a83cbe71a63f7e025a67f08ab8ed9ddd6191e4
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-20 21:44:55 -0500

    Error white

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import App from './routes/App'
+import App from './routes/App'; 
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 root.render = (<App />);
+
 // ReactDOM.render(<App />, document.getElementById('app'));
