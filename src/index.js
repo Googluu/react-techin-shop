dcecf27fe905de955e8d2a2ab34274d4ece7105e
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-21 15:07:52 -0500

    Err 404

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './routes/App'; 
 
 const container = document.getElementById('app');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render = (<App />);
 
 // ReactDOM.render(<App />, document.getElementById('app'));
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Layout from '../containers/Layout';
-import Login from '../containers/login';
+import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 
 const App = () => {
     return (
-      <BrowserRouter>
+      <Router>
         <Layout>
             <Routes>
-                <Route exact path="/" component={<Home />} />
-                <Route exact path="/login"
-                component={<Login />} />
-                <Route exact path="/recovery-password"
-                component={<RecoveryPassword />} />
-                <Route path="/" component={<NotFound />} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/recovery-password" component={RecoveryPassword} />
+                <Route path="*" component={NotFound} />
             </Routes>
         </Layout>
-      </BrowserRouter>
+      </Router>
     );
 }
 
